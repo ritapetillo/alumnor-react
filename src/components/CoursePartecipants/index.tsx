a2585f1aa7f6314ff10a205669fb7c7b6f80b374
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-04-09 16:31:29 -0400

    menuforstudents

--- src/components/CoursePartecipants/index.tsx
+++ src/components/CoursePartecipants/index.tsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { AiOutlineMail } from "react-icons/ai";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { getAllStudentsPerCourse } from "../../api/userApi";
 import IUser from "../../interfaces/IUser";
+import isInstructor from "../../libs/isInstructor";
+import { RootStore } from "../../store";
 import { SpanLink, Table } from "../../styles/uiKit";
 import { CoursePartecipantsWrapper } from "./ coursepartecipants.elements";
 const CoursePartecipants = () => {
   const params: { id: string } = useParams();
   const [students, setStudents] = useState<IUser[] | undefined>();
-
+  const currentUser = useSelector((state: RootStore) => state.auth.user);
+  const currentCourse = useSelector(
+    (state: RootStore) => state.courses.currentCourse
+  );
+  const [isStudent, setIsStudent] = useState(true);
   useEffect(() => {
     getAllStudents();
-  }, [params]);
+    setIsStudent(!isInstructor(currentUser,currentCourse))
+  }, [params,currentUser._id]);
 
   const getAllStudents = async () => {
     const students = await getAllStudentsPerCourse(params.id);
@@ -20,15 +28,15 @@ const CoursePartecipants = () => {
 
   return (
     <CoursePartecipantsWrapper>
-      <h2>Students</h2>
+      <h2>Course Partecipants</h2>
       <Table>
         <thead>
           <tr>
             <th></th>
             <th>Name</th>
             <th>Last Name</th>
-            <th>Email</th>
-            <th>Enrollments</th>
+            {!isStudent &&<> <th>Email</th>
+            <th>Enrollments</th></>}
             <th></th>
           </tr>
         </thead>
@@ -38,15 +46,19 @@ const CoursePartecipants = () => {
               <th>{i + 1}</th>
               <th>{student.firstName}</th>
               <th>{student.lastName}</th>
-              <th>{student.email}</th>
-              <th>
-                {student.enrollments.map((enrollment: any) => (
-                  <>
-                    {" "}
-                    <SpanLink>{enrollment.courseId.title}</SpanLink>{" "}
-                  </>
-                ))}
-              </th>
+              {!isStudent && (
+                <>
+                  <th>{student.email}</th>
+                  <th>
+                    {student.enrollments.map((enrollment: any) => (
+                      <>
+                        {" "}
+                        <SpanLink>{enrollment.courseId.title}</SpanLink>{" "}
+                      </>
+                    ))}
+                  </th>
+                </>
+              )}
               <th>
                 <AiOutlineMail />
               </th>
--- src/components/MenuCourse/index.tsx
+++ src/components/MenuCourse/index.tsx
@@ -4,12 +4,19 @@ import React, {
   PropsWithRef,
   ReactNode,
   Ref,
+  useEffect,
+  useMemo,
+  useState,
 } from "react";
 import { MenuCourseWrapper } from "./menucourse.elements";
 import { AiFillDelete, AiFillHome, AiFillSetting } from "react-icons/ai";
 import { HiOutlineUserGroup, HiUserGroup } from "react-icons/hi";
 import { RiBookletFill } from "react-icons/ri";
 import { NavLink, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootStore } from "../../store";
+import { ISubmission } from "../../interfaces/redux/states/ICourseInitialState";
+import isInstructor from "../../libs/isInstructor";
 
 interface Props {
   children?: ReactNode;
@@ -19,6 +26,67 @@ interface Props {
 const MenuCourse = React.forwardRef<HTMLDivElement, Props>(
   ({ handleClose }, ref) => {
     const params: { courseId: string } = useParams();
+    const submissions = useSelector(
+      (state: RootStore) => state.courses.currentCourseSubmissions
+    );
+    const currentUser = useSelector((state: RootStore) => state.auth.user);
+    const currentCourse = useSelector(
+      (state: RootStore) => state.courses.currentCourse
+    );
+    const [newSubmissions, setNewSubmissions] = useState(0);
+    const [isStudent, setIsStudent] = useState(true);
+
+    useEffect(() => {
+      if (submissions) {
+        const newSubmissionsNumber = submissions.filter(
+          (sub: ISubmission) => sub.checked === false
+        ).length;
+        setNewSubmissions(newSubmissionsNumber);
+      }
+      if (currentUser._id) {
+        setIsStudent(!isInstructor(currentUser, currentCourse));
+      }
+    }, [submissions, currentUser._id]);
+
+    const menuToShow = useMemo(() => {
+      if (isStudent) {
+        return (
+          <>
+            {" "}
+            {/* <NavLink to={`submissions`} onClick={handleClose}> */}
+            <div>
+              <span>My Homework</span>
+
+              <RiBookletFill />
+            </div>
+            {/* </NavLink> */}
+          </>
+        );
+      } else {
+        return (
+          <>
+            {" "}
+            <NavLink to={`submissions`} onClick={handleClose}>
+              <div>
+                <span>Assignments </span>
+                {newSubmissions}
+                <RiBookletFill />
+              </div>
+            </NavLink>
+            <NavLink to={`edit`} onClick={handleClose}>
+              <div>
+                <span>Settings</span>
+                <AiFillSetting />
+              </div>
+            </NavLink>
+            <div>
+              <span>Delete Course</span>
+              <AiFillDelete />
+            </div>
+          </>
+        );
+      }
+    }, [currentUser, isStudent]);
     return (
       <MenuCourseWrapper ref={ref}>
         <NavLink to={`main`}>
@@ -33,22 +101,7 @@ const MenuCourse = React.forwardRef<HTMLDivElement, Props>(
             <HiUserGroup />
           </div>
         </NavLink>
-         <NavLink to={`submissions`} onClick={handleClose}>
-        <div>
-          <span>Assignments</span>
-          <RiBookletFill />
-        </div>
-        </NavLink>
-        <NavLink to={`edit`} onClick={handleClose}>
-          <div>
-            <span>Settings</span>
-            <AiFillSetting />
-          </div>
-        </NavLink>
-        <div>
-          <span>Delete Course</span>
-          <AiFillDelete />
-        </div>
+        {menuToShow}
       </MenuCourseWrapper>
     );
   }
