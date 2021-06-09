0ccb5503838ee03ba70ccffc7aa8c22aa71b35c9
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-06-09 12:11:45 -0400

    delete connection to socket

--- src/context/socketContext/index.tsx
+++ src/context/socketContext/index.tsx
@@ -6,33 +6,33 @@ import { RootStore } from "../../store";
 
 export const SocketContext = React.createContext({});
 const Socket: FC = ({ children }) => {
-  const currentUserId = useSelector((state: RootStore) => state.auth.user._id);
-  //   const [socket] = useSocket(process.env.REACT_APP_SOCKET_URI);
-  const socket = io("http://localhost:3002");
-  const connectToSocket = () => {
-    socket.auth = { currentUserId: currentUserId };
-    socket.connect();
-  };
+  // const currentUserId = useSelector((state: RootStore) => state.auth.user._id);
+  // //   const [socket] = useSocket(process.env.REACT_APP_SOCKET_URI);
+  // const socket = io("http://localhost:3002");
+  // const connectToSocket = () => {
+  //   socket.auth = { currentUserId: currentUserId };
+  //   socket.connect();
+  // };
 
-  useEffect(() => {
-    //  connectToSocket()
-    //   socket.on("connection", () => {
-    //     console.log("connected");
-    //     socket.emit("setId", { id: currentUserId });
-    //   });
-    //   socket.on('users',(users)=>console.log(users))
-    //   socket.on("connect_error", (err) => {
-    //     if (err.message === "invalid username") {
-    //       console.log('invalid username')
-    //     }
-    //   });
-    //   return () => {
-    //     socket.on("disconnect", () => console.log("disconnected"));
-    //   };
-  }, [currentUserId]);
+  // useEffect(() => {
+  //   //  connectToSocket()
+  //   //   socket.on("connection", () => {
+  //   //     console.log("connected");
+  //   //     socket.emit("setId", { id: currentUserId });
+  //   //   });
+  //   //   socket.on('users',(users)=>console.log(users))
+  //   //   socket.on("connect_error", (err) => {
+  //   //     if (err.message === "invalid username") {
+  //   //       console.log('invalid username')
+  //   //     }
+  //   //   });
+  //   //   return () => {
+  //   //     socket.on("disconnect", () => console.log("disconnected"));
+  //   //   };
+  // }, [currentUserId]);
 
   return (
-    <SocketContext.Provider value={"rits"}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={"try"}>{children}</SocketContext.Provider>
   );
 };
 
